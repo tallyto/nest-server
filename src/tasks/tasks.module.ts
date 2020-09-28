eedd932c682e6e898e5943e7772cd56e18652e65
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2020-09-28 14:01:02 -0400

    feat: add mongoose

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TaskService } from './shared/task.service';
-
+import {TaskSchema} from './schemas/task.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
+  imports: [MongooseModule.forFeature([{name: 'Task', schema: TaskSchema}])],
   controllers: [TasksController],
   providers: [TaskService]
 })
